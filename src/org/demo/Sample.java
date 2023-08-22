a205d4e18076edee75ff23ba965da1f7556095ed
riyasri1166 <riyasri1166@gmail.com>
2023-08-22 15:41:39 +0530

    succes

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -3,6 +3,7 @@ package org.demo;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hi good Test 1 Dummy Project");
+		System.out.println("Hi good Test 1 dev Project");
 	}
 
 }
